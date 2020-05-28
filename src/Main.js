4119d14eeabe52d43340c066f6a8e7a9f7cf0b37
WiranataK <wiranatak99@gmail.com>
2020-05-28 20:19:07 +0800

    Update Main.js
    Add Route Partner Communication and Member Screen access table

--- src/Main.js
+++ src/Main.js
@@ -5,19 +5,20 @@ import { Sidebar } from './Page/Sidebar';
 import { Tambah_Data } from './Page/Tambah_Data';
 import { Identification_Screen } from './Page/Identification_Screen';
 import { Partner_Address } from './Page/Partner_Address';
+import { Partner_Communication } from './Page/Partner_Communication';
+import { Member_Screen } from './Page/Member_Screen';
 
 function Main() {
   return (
     <React.Fragment>
       <Sidebar />
       <Tambah_Data />
-      {/* <Identification_Screen/> */}
       <Router>
         <Switch>
           <Route exact path="/Identification_Screen" component={Identification_Screen} />
           <Route path="/Partner_Address" component={Partner_Address} />
-          {/* <Route path="/Partner_Communication" component={Partner_Communication} />
-          <Route path="/Member_Screen" component={Member_Screen} /> */}
+          <Route path="/Partner_Communication" component={Partner_Communication} />
+          <Route path="/Member_Screen" component={Member_Screen} />
         </Switch>
       </Router>
     </React.Fragment>
