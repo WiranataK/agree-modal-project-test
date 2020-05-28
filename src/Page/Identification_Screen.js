9a7fa7276a01c6b64754b62fa7928b6db19b864b
WiranataK <wiranatak99@gmail.com>
2020-05-28 17:14:32 +0800

    Identification Page
    Done but Minus looping table

---
+++ src/Page/Identification_Screen.js
@@ -0,0 +1,123 @@
+import React from 'react';
+import { Table } from 'react-bootstrap';
+import './css/Table_CSS.css';
+
+export const Identification_Screen = () => (
+    <React.Fragment>
+        <h2 className="header">Daftar Identification Screen</h2>
+        <Table className="istable" responsive striped hover variant="light">
+            <thead className="head">
+                <tr>
+                    <th>No</th>
+                    <th>Identification Type Name</th>
+                    <th>Identification Number</th>
+                    <th>Date of Issue</th>
+                    <th>Date of Expire</th>
+                    <th>Place of Issue</th>
+                    <th>Country of Issue</th>
+                </tr>
+            </thead>
+            <tbody className="body">
+                <tr>
+                    <th>1</th>
+                    <th>XXXXXX</th>
+                    <th>XXXX</th>
+                    <th>XX/XX/XXXX</th>
+                    <th>XX/XX/XXXX</th>
+                    <th>XXXXXX</th>
+                    <th>XXXXXXXX</th>
+                </tr>
+                <tr>
+                    <th>1</th>
+                    <th>XXXXXX</th>
+                    <th>XXXX</th>
+                    <th>XX/XX/XXXX</th>
+                    <th>XX/XX/XXXX</th>
+                    <th>XXXXXX</th>
+                    <th>XXXXXXXX</th>
+                </tr>
+                <tr>
+                    <th>1</th>
+                    <th>XXXXXX</th>
+                    <th>XXXX</th>
+                    <th>XX/XX/XXXX</th>
+                    <th>XX/XX/XXXX</th>
+                    <th>XXXXXX</th>
+                    <th>XXXXXXXX</th>
+                </tr>
+                <tr>
+                    <th>1</th>
+                    <th>XXXXXX</th>
+                    <th>XXXX</th>
+                    <th>XX/XX/XXXX</th>
+                    <th>XX/XX/XXXX</th>
+                    <th>XXXXXX</th>
+                    <th>XXXXXXXX</th>
+                </tr>
+                <tr>
+                    <th>1</th>
+                    <th>XXXXXX</th>
+                    <th>XXXX</th>
+                    <th>XX/XX/XXXX</th>
+                    <th>XX/XX/XXXX</th>
+                    <th>XXXXXX</th>
+                    <th>XXXXXXXX</th>
+                </tr>
+                <tr>
+                    <th>1</th>
+                    <th>XXXXXX</th>
+                    <th>XXXX</th>
+                    <th>XX/XX/XXXX</th>
+                    <th>XX/XX/XXXX</th>
+                    <th>XXXXXX</th>
+                    <th>XXXXXXXX</th>
+                </tr>
+                <tr>
+                    <th>1</th>
+                    <th>XXXXXX</th>
+                    <th>XXXX</th>
+                    <th>XX/XX/XXXX</th>
+                    <th>XX/XX/XXXX</th>
+                    <th>XXXXXX</th>
+                    <th>XXXXXXXX</th>
+                </tr>
+                <tr>
+                    <th>1</th>
+                    <th>XXXXXX</th>
+                    <th>XXXX</th>
+                    <th>XX/XX/XXXX</th>
+                    <th>XX/XX/XXXX</th>
+                    <th>XXXXXX</th>
+                    <th>XXXXXXXX</th>
+                </tr>
+                <tr>
+                    <th>1</th>
+                    <th>XXXXXX</th>
+                    <th>XXXX</th>
+                    <th>XX/XX/XXXX</th>
+                    <th>XX/XX/XXXX</th>
+                    <th>XXXXXX</th>
+                    <th>XXXXXXXX</th>
+                </tr>
+                <tr>
+                    <th>1</th>
+                    <th>XXXXXX</th>
+                    <th>XXXX</th>
+                    <th>XX/XX/XXXX</th>
+                    <th>XX/XX/XXXX</th>
+                    <th>XXXXXX</th>
+                    <th>XXXXXXXX</th>
+                </tr>
+                <tr>
+                    <th>1</th>
+                    <th>XXXXXX</th>
+                    <th>XXXX</th>
+                    <th>XX/XX/XXXX</th>
+                    <th>XX/XX/XXXX</th>
+                    <th>XXXXXX</th>
+                    <th>XXXXXXXX</th>
+                </tr>
+            </tbody>
+        </Table>
+    </React.Fragment>
+)
