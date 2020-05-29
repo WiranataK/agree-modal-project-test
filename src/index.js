c488654c7602dfc52abfd1440cedc1c7d76eab9e
WiranataK <wiranatak99@gmail.com>
2020-05-29 17:36:16 +0800

    Update index.js
    Test Login Page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Main from './Main';
+import Login from './Page/Login';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Main />
+    <Login/>
   </React.StrictMode>,
   document.getElementById('root')
 );
