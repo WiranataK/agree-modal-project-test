003d93d6366cd45d46b969151eb2508e1b98cd97
WiranataK <wiranatak99@gmail.com>
2020-05-29 17:34:55 +0800

    Add Login Page
    All DONE with show and hide password. Minus Credentials Error

---
+++ src/Page/Login.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import './css/Login.css';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import Logo from '../Assets/logo.png';
+import FarmerLogo from '../Assets/farmer.png';
+import { icons } from 'react-icons/lib/cjs';
+
+export default class Login extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            type: 'password',
+        }
+        this.showHide = this.showHide.bind(this);
+    }
+
+    showHide(e){
+        e.preventDefault();
+        e.stopPropagation();
+        this.setState({
+            type: this.state.type === 'password' ? 'text' : 'password'
+        })  
+    }
+
+    render(){
+        return(
+            <React.Fragment>
+                <Container id="container">
+                    <Row>
+                        <Col>
+                            <h1>Selamat datang di</h1>
+                            <img id="logo" src={Logo}/>
+                            <Form>
+                                <Form.Group>
+                                    <Form.Control id="username" type="username" placeholder="Username"></Form.Control>
+                                    <Form.Control id="password" type={this.state.type} placeholder="Password"></Form.Control>
+                                    <icons id="showhide" onClick={this.showHide}>
+                                        {this.state.type === 'password' ? <FaEye/> : <FaEyeSlash/>}
+                                    </icons>
+                                    <Button id="buttonmasuk" variant="primary" type="submit">Masuk</Button>
+                                </Form.Group>
+                            </Form>
+                            <h2>Belum punya akun?</h2>
+                            <Button id="buttonregister" variant="link">Daftar di sini</Button>
+                        </Col>
+                        <Col>
+                            <img id="farmerlogo" src={ FarmerLogo }/>
+                        </Col>
+                    </Row>
+                </Container>
+            </React.Fragment>
+        );
+    }
+}
