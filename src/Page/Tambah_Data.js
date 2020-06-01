2a7db8d91d6a8d1c1ac37e0269ff49989efe6d2c
WiranataK <wiranatak99@gmail.com>
2020-06-01 19:23:42 +0800

    Move File
    Move to Another Folder

--- src/Page/Tambah_Data.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
-import './css/Tambah_Data.css';
-
-export const Tambah_Data = () => (
-    <React.Fragment className="back">
-        <Button id="buttonadddata" variant="primary">Tambah Data</Button>
-    </React.Fragment>
-)
