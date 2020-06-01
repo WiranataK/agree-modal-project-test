119e260a39a71bc4bd71b03bc045d512eb7052cc
WiranataK <wiranatak99@gmail.com>
2020-06-01 19:27:01 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Login from './Page/Login';
+import Registrasi from './Page/Components/Registrasi';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Login/>
+    <Registrasi/>
   </React.StrictMode>,
   document.getElementById('root')
 );
