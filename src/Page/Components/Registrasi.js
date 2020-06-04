470e86d56050bb8065bbe41b071f153da4423cbc
WiranataK <wiranatak99@gmail.com>
2020-06-04 17:18:53 +0800

    Make Radio Button
    Still error and i dont know why

--- src/Page/Components/Registrasi.js
+++ src/Page/Components/Registrasi.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import '../css/Registrasi.css';
-import { Container, Button } from 'react-bootstrap';
+import { Container, Button, ButtonGroup, ToggleButton, ToggleButtonGroup } from 'react-bootstrap';
 
 export default class Registrasi extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            disabled: true,
+        }
+        this.disabledButton = this.disabledButton.bind(this);
+    }
 
+    disabledButton(e){
+        e.preventDefault();
+        e.stopPropagation();
+        this.setState({
+            disabled: this.state.disabled === true ? false : true
+        })
+    }
 
     render(){
         return(
@@ -11,14 +25,21 @@ export default class Registrasi extends React.Component{
                 <Container id="container">
                     <h1 id="headert1">Pilih Jenis Member Agree</h1>
                         <Container id="container2">
-                            <Button id="option" variant="outline-primary">Individu</Button>{' '}
-                            <Button id="option" variant="outline-primary">Individu Berusaha</Button>{' '}
-                            <Button id="option" variant="outline-primary">Perusahaan / UKM</Button>{' '}
-                            <Button id="option" variant="outline-primary">Offtaker</Button>{' '}
-                            <Button id="option" variant="outline-primary">FA Offtaker</Button>{' '}
-                            <Button id="option" variant="outline-primary">FA Lembaga Keuangan</Button>{' '}
+                            <ToggleButtonGroup name="radio" type="radio" id="buttongroup" vertical>
+                                <ToggleButton id="option" variant="outline-primary" onFocus={this.disabledButton}>Individu</ToggleButton>
+                                <ToggleButton id="option" variant="outline-primary" onFocus={this.disabledButton}>Individu Berusaha</ToggleButton>
+                                <ToggleButton id="option" variant="outline-primary" onFocus={this.disabledButton}>Perusahaan / UKM</ToggleButton>
+                                <ToggleButton id="option" variant="outline-primary" onFocus={this.disabledButton}>Offtaker</ToggleButton>
+                                <ToggleButton id="option" variant="outline-primary" onFocus={this.disabledButton}>FA Offtaker</ToggleButton>
+                                <ToggleButton id="option" variant="outline-primary" onFocus={this.disabledButton}>FA Lembaga Keuangan</ToggleButton>
+                            </ToggleButtonGroup>
+                            <ToggleButtonGroup type="checkbox" className="mb-2">
+                                <ToggleButton value={1}>Checkbox 1 (pre-checked)</ToggleButton>
+                                <ToggleButton value={2}>Checkbox 2</ToggleButton>
+                                <ToggleButton value={3}>Checkbox 3 (pre-checked)</ToggleButton>
+                            </ToggleButtonGroup>
                         </Container>
-                    <Button id="button" variant="primary">Selanjutnya</Button>
+                    <Button id="button" variant="primary" disabled={this.state.disabled}>Selanjutnya</Button>
                 </Container>
             </React.Fragment>
         );
