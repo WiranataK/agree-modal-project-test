fd200e4ee2219b23bc9b8235b6659531919d6ea3
WiranataK <wiranatak99@gmail.com>
2020-06-05 14:34:59 +0800

    Update Registrasi.js
    change toggle button to basic button but still error

--- src/Page/Components/Registrasi.js
+++ src/Page/Components/Registrasi.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../css/Registrasi.css';
-import { Container, Button, ButtonGroup, ToggleButton, ToggleButtonGroup } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
 
 export default class Registrasi extends React.Component{
     constructor(props){
@@ -15,7 +15,7 @@ export default class Registrasi extends React.Component{
         e.preventDefault();
         e.stopPropagation();
         this.setState({
-            disabled: this.state.disabled === true ? false : true
+            disabled: this.state.disabled === true ? false : false
         })
     }
 
@@ -25,19 +25,12 @@ export default class Registrasi extends React.Component{
                 <Container id="container">
                     <h1 id="headert1">Pilih Jenis Member Agree</h1>
                         <Container id="container2">
-                            <ToggleButtonGroup name="radio" type="radio" id="buttongroup" vertical>
-                                <ToggleButton id="option" variant="outline-primary" onFocus={this.disabledButton}>Individu</ToggleButton>
-                                <ToggleButton id="option" variant="outline-primary" onFocus={this.disabledButton}>Individu Berusaha</ToggleButton>
-                                <ToggleButton id="option" variant="outline-primary" onFocus={this.disabledButton}>Perusahaan / UKM</ToggleButton>
-                                <ToggleButton id="option" variant="outline-primary" onFocus={this.disabledButton}>Offtaker</ToggleButton>
-                                <ToggleButton id="option" variant="outline-primary" onFocus={this.disabledButton}>FA Offtaker</ToggleButton>
-                                <ToggleButton id="option" variant="outline-primary" onFocus={this.disabledButton}>FA Lembaga Keuangan</ToggleButton>
-                            </ToggleButtonGroup>
-                            <ToggleButtonGroup type="checkbox" className="mb-2">
-                                <ToggleButton value={1}>Checkbox 1 (pre-checked)</ToggleButton>
-                                <ToggleButton value={2}>Checkbox 2</ToggleButton>
-                                <ToggleButton value={3}>Checkbox 3 (pre-checked)</ToggleButton>
-                            </ToggleButtonGroup>
+                            <Button id="option" name="option" variant="outline-primary" onFocus={this.disabledButton}>Individu</Button>{' '}
+                            <Button id="option" name="option" variant="outline-primary" onFocus={this.disabledButton}>Individu Berusaha</Button>{' '}
+                            <Button id="option" name="option" variant="outline-primary" onFocus={this.disabledButton}>Perusahaan / UKM</Button>{' '}
+                            <Button id="option" name="option" variant="outline-primary" onFocus={this.disabledButton}>Offtaker</Button>{' '}
+                            <Button id="option" name="option" variant="outline-primary" onFocus={this.disabledButton}>FA Offtaker</Button>{' '}
+                            <Button id="option" name="option" variant="outline-primary" onFocus={this.disabledButton}>FA Lembaga Keuangan</Button>{' '}
                         </Container>
                     <Button id="button" variant="primary" disabled={this.state.disabled}>Selanjutnya</Button>
                 </Container>
