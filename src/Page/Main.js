f77dbd584ffa50e03d68fb928974004f0f371f3e
WiranataK <wiranatak99@gmail.com>
2020-07-09 13:10:41 +0800

    Move Main.js to folder Page

---
+++ src/Page/Main.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Sidebar } from './Components/Sidebar';
+import { Tambah_Data } from './Components/Tambah_Data';
+import { Identification_Screen } from './Components/Identification_Screen';
+import { Partner_Address } from './Components/Partner_Address';
+import { Partner_Communication } from './Components/Partner_Communication';
+import { Member_Screen } from './Components/Member_Screen';
+
+function Main() {
+  return (
+    <React.Fragment>
+      <Sidebar />
+      <Router>
+        <Switch>
+          <Route exact path="/Identification_Screen" component={Identification_Screen} />
+          <Route path="/Partner_Address" component={Partner_Address} />
+          <Route path="/Partner_Communication" component={Partner_Communication} />
+          <Route path="/Member_Screen" component={Member_Screen} />
+        </Switch>
+      </Router>
+    </React.Fragment>
+  );
+}
+
+export default Main;
