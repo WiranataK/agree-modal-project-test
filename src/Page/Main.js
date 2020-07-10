a39951e73279397516d0c7c95bf18bf0f79a8dca
WiranataK <wiranatak99@gmail.com>
2020-07-10 15:39:00 +0800

    Update Main.js

--- src/Page/Main.js
+++ src/Page/Main.js
@@ -6,19 +6,21 @@ import { Identification_Screen } from './Components/Identification_Screen';
 import { Partner_Address } from './Components/Partner_Address';
 import { Partner_Communication } from './Components/Partner_Communication';
 import { Member_Screen } from './Components/Member_Screen';
+import { Table_Partner } from './Components/Table_Partner';
 
 function Main() {
   return (
     <React.Fragment>
       <Sidebar />
-      <Router>
+      <Table_Partner/>
+      {/* <Router>
         <Switch>
           <Route exact path="/Identification_Screen" component={Identification_Screen} />
           <Route path="/Partner_Address" component={Partner_Address} />
           <Route path="/Partner_Communication" component={Partner_Communication} />
           <Route path="/Member_Screen" component={Member_Screen} />
         </Switch>
-      </Router>
+      </Router> */}
     </React.Fragment>
   );
 }
