3ad23dfbfdfdd232607daf30e0fefa252f5d0320
Lazuardi Firdaus <lazuardifirdaus369@yahoo.com>
2020-07-28 16:04:53 +0700

    fixed merge mistake

--- src/Page/Keanggotaan.js
+++ src/Page/Keanggotaan.js
@@ -1,26 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Sidebar } from './Components/Sidebar';
-<<<<<<< HEAD
 import { Tambah_Data } from './Components/Tambah_Data';
 import { Form_Tambah } from './Components/Form_Tambah';
-=======
->>>>>>> 28f6dbbf79b20e213edd154e200d519cc589484d
 import { Table_Partner } from './Components/Table_Partner';
 import Check_Token from './Components/Check_Token';
 
 function Keanggotaan() {
   return (
     <React.Fragment>
-<<<<<<< HEAD
+      <Check_Token />
       {/* <Sidebar />
       <Table_Partner/> */}
       <Form_Tambah/>
-=======
-      <Check_Token />
-      <Sidebar />
-      <Table_Partner/>
->>>>>>> 28f6dbbf79b20e213edd154e200d519cc589484d
       {/* <Router>
         <Switch>
           <Route exact path="/Identification_Screen" component={Identification_Screen} />
