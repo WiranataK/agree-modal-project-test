a3be5f7fdb731816c1ba025a05e6b8a607f7f035
WiranataK <wiranatak99@gmail.com>
2020-07-28 12:14:05 +0800

    Update Keanggotaan.js

--- src/Page/Keanggotaan.js
+++ src/Page/Keanggotaan.js
@@ -2,17 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Sidebar } from './Components/Sidebar';
 import { Tambah_Data } from './Components/Tambah_Data';
-import { Identification_Screen } from './Components/Identification_Screen';
-import { Partner_Address } from './Components/Partner_Address';
-import { Partner_Communication } from './Components/Partner_Communication';
-import { Member_Screen } from './Components/Member_Screen';
+import { Form_Tambah } from './Components/Form_Tambah';
 import { Table_Partner } from './Components/Table_Partner';
 
 function Keanggotaan() {
   return (
     <React.Fragment>
-      <Sidebar />
-      <Table_Partner/>
+      {/* <Sidebar />
+      <Table_Partner/> */}
+      <Form_Tambah/>
       {/* <Router>
         <Switch>
           <Route exact path="/Identification_Screen" component={Identification_Screen} />
