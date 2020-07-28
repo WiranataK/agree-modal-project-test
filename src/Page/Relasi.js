8df0617ee5cc8eec6615034bb53168f3372e31b3
WiranataK <wiranatak99@gmail.com>
2020-07-28 12:16:51 +0800

    Create Relasi.js

---
+++ src/Page/Relasi.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Sidebar } from './Components/Sidebar';
+import { Table_Relasi } from './Components/Table_Relasi';
+import Check_Token from './Components/Check_Token';
+
+function Relasi() {
+  return (
+    <React.Fragment>
+      <Check_Token />
+      <Sidebar />
+      <Table_Relasi/>
+      {/* <Router>
+        <Switch>
+          <Route exact path="/Identification_Screen" component={Identification_Screen} />
+          <Route path="/Partner_Address" component={Partner_Address} />
+          <Route path="/Partner_Communication" component={Partner_Communication} />
+          <Route path="/Member_Screen" component={Member_Screen} />
+        </Switch>
+      </Router> */}
+    </React.Fragment>
+  );
+}
+
+export default Relasi;
