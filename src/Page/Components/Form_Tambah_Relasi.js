c0929ac939a8f6e8b9f8d19f8f72728a192d43f2
WiranataK <wiranatak99@gmail.com>
2020-08-04 03:39:36 +0800

    Create Form_Tambah_Relasi.js

---
+++ src/Page/Components/Form_Tambah_Relasi.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import '../css/Form.css';
+import { Modal, Button, Row, Col, Form,  } from 'react-bootstrap';
+
+export const Form_Tambah_Relasi = () => (
+    <Modal.Dialog size="lg">
+        <Modal.Header closeButton>
+            <Modal.Title id="header">Tambah Data Relasi</Modal.Title>
+        </Modal.Header>
+        <Modal.Body id="body">
+            <Row>
+                <Col>
+                    <Form.Label>Parent Partner</Form.Label>
+                    <Form.Control as="select" placeholder="Pilih Parent Partner">
+                        <option>Pilih Parent Partner</option>
+                        <option>...</option>
+                    </Form.Control>
+                </Col>
+                <Col>
+                    <Form.Label>Child Partner</Form.Label>
+                    <Form.Control as="select" placeholder="Pilih Child Partnern">
+                        <option>Pilih Child Partner</option>
+                        <option>...</option>
+                    </Form.Control>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Form.Label>Tipe Relasi</Form.Label>
+                    <Form.Control as="select" placeholder="Pilih Tipe Relasi">
+                        <option>Pilih Tipe Relasi</option>
+                        <option>...</option>
+                    </Form.Control>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Button id="btnprimary">
+                        <Col xs={"auto"}>
+                        </Col>
+                        <Col xs={10}>Tambah Data</Col>
+                    </Button>
+                </Col>
+            </Row>
+        </Modal.Body>
+    </Modal.Dialog>
+)
